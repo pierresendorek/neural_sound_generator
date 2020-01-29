9afab8a87704738e433753fd07aa3bc114fbefaf
pierresendorek <pierresendorek@gmail.com>
2020-01-29 23:24:31 +0100

    batch generator from real sounds

--- src/gan/training.py
+++ src/gan/training.py
@@ -1,8 +1,9 @@
+from src.gan.batch_generator import BatchGenerator
 from src.gan.discriminator import Discriminator
 from src.gan.generator import Generator
 import numpy as np
 import tensorflow as tf
-
+import src.init_cudnn # keep this import for CuDNN to work
 
 window_len = 1024
 
@@ -10,14 +11,9 @@ discriminator = Discriminator()
 generator = Generator(window_len)
 
 
-def generate_batch(batch_size, window_len):
-    sound_prev = np.random.randn(batch_size, window_len, 1).astype(np.float32)
+def generate_batch(sound_prev, sound_curr):
     artificial = generator(sound_prev)
-    natural = np.random.randn(batch_size, window_len, 1).astype(np.float32)
-
-    #labels = np.concatenate([np.zeros([batch_size, 1]), np.ones([batch_size, 1])], axis=0).astype(np.float32)
-    #sounds = np.concatenate([natural, artificial], axis=0).astype(np.float32)
-
+    natural = sound_curr
     return sound_prev, natural, artificial
 
 
@@ -25,32 +21,55 @@ def mean_square(x):
     return tf.reduce_mean(tf.square(x))
 
 def discriminator_loss(sound_prev, natural, artificial):
-    return mean_square(discriminator.call(sound_prev, natural)) + \
-           mean_square(discriminator.call(sound_prev, artificial) - 1.0)
+    d_natural = discriminator.call(sound_prev, natural)
+    d_artificial = discriminator.call(sound_prev, artificial)
+    loss = mean_square(d_natural) + \
+           mean_square(d_artificial - tf.ones_like(d_artificial))
+    return loss, (d_natural, d_artificial)
 
 def generator_loss(sound_prev, artificial):
     return mean_square(discriminator.call(sound_prev, artificial))
 
 
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+generator_optimizer = tf.keras.optimizers.Adam(1e-5)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-5)
 
-def train_step():
+batch_generator = BatchGenerator()
 
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      sound_prev, natural, artificial = generate_batch(batch_size=10, window_len=1024)
-      gen_loss = generator_loss(sound_prev, artificial)
-      disc_loss = discriminator_loss(sound_prev, natural, artificial)
+nb_steps_train = 100
 
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+def train_step(nb_steps_train):
 
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    for _ in range(nb_steps_train):
+        with tf.GradientTape() as disc_tape:
+            sound_prev, sound_curr = batch_generator.draw_batch(window_len, 10)
+            sound_prev, natural, artificial = generate_batch(sound_prev, sound_curr)
+            disc_loss, res = discriminator_loss(sound_prev, natural, artificial)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+        print(res)
+
+    for _ in range(nb_steps_train):
+        with tf.GradientTape() as gen_tape:
+          sound_prev, sound_curr = batch_generator.draw_batch(window_len, 10)
+          sound_prev, natural, artificial = generate_batch(sound_prev, sound_curr)
+          gen_loss = generator_loss(sound_prev, artificial)
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
 
 
-for i in range(100):
+
+
+
+
+
+
+
+
+
+
+for i in range(10**6):
+    train_step(100)
     print(i)
-    train_step()
 
 
